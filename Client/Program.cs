4780629d655b8c5171cb2ecc70f17757c4ac0d5a
aaa <aa@aa.aa>
2020-10-07 18:06:39 +0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buffer = new byte[1024];
+            int count;
+            Socket socket;
+
+            Console.WriteLine("Введите имя пользователя");
+            while (true)
+            {
+                try
+                {
+                    string name = Console.ReadLine();
+
+                    socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect("127.0.0.1", 1023);
+
+                    socket.Send(Encoding.UTF8.GetBytes('\0' + name));
+                    count = socket.Receive(buffer);
+
+                    if (count == 1 && buffer[0] == 1)
+                        break;
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Ошибка ввода. Введите еще раз");
+                }
+            }
+
+            Task.Run(() => MassegeHandler(socket));
+
+            Console.WriteLine("Для выхода из чата нажмите Ctrl+C");
+            Console.CancelKeyPress += (s, e) => 
+            {
+                socket.Disconnect(false);
+                Environment.Exit(0);
+            };
+
+            string message;
+            while(true)
+            {
+                message = Console.ReadLine();
+                if(message != null)
+                    socket.Send(Encoding.UTF8.GetBytes('\0' + message));
+            }
+        }
+
+        private static void MassegeHandler(Socket socket)
+        {
+            byte[] buffer = new byte[1024];
+            int count;
+
+            try
+            {
+                while (true)
+                {
+                    count = socket.Receive(buffer);
+                    if (buffer[0] == 0)
+                    {
+                        socket.Send(buffer);
+                        continue;
+                    }
+                    Console.WriteLine(Encoding.UTF8.GetString(buffer, 1, count - 1));
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Соединение с сервером потеряно");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static Dictionary<Socket, string> users = new Dictionary<Socket, string>();
+        static TcpListener listener= new TcpListener(IPAddress.Parse("127.0.0.1"), 1023);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запуск сервера");
+
+            listener.Start();
+            Task task = new Task(SocketHandler);
+            task.Start();
+
+            while (true)
+            {
+                string stop = Console.ReadLine();
+                switch (stop.ToLower())
+                {
+                    case "stop":
+                        listener.Stop();
+                        return;
+                    case "users":
+                        Console.WriteLine(string.Join( Environment.NewLine, users.Values.ToArray()));
+                        break;
+                    case "clean":
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+
+        private static void SocketHandler()
+        {
+            while (true)
+            {
+                Socket s = listener.AcceptSocket();
+                Task.Run(() => MessageHandler(s) );
+            }
+        }
+
+        private static void MessageHandler(Socket socket)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int count = socket.Receive(buffer);
+
+                string name = Encoding.UTF8.GetString(buffer, 1, count - 1);
+
+                if (users.ContainsValue(name))
+                {
+                    socket.Disconnect(false);
+                    Console.WriteLine($"Совпадение имени {name}");
+                    return;
+                }
+
+                Console.WriteLine($"Подключился пользователь {name}");
+                socket.Send(new byte[] { 1 });
+
+                users.Add(socket, name);
+                while (true)
+                {
+                    count = socket.Receive(buffer);
+                    if (count == 0)
+                        continue;
+
+                    if (buffer[0] != 0)
+                    {
+                        socket.Send(buffer, count, SocketFlags.None);
+                        continue;
+                    }
+
+                    string message = '$' + name + ": " + Encoding.UTF8.GetString(buffer, 1, count - 1);
+                    Console.WriteLine($"Сообщение от {message.Remove(0,1)}");
+
+                    foreach (Socket s in users.Keys)
+                        s.Send(Encoding.UTF8.GetBytes(message));
+                }
+            }
+            catch (SocketException)
+            {
+                users.Remove(socket);
+            }
+        }
+    }
+}
